e8a485b7a77ae17a5657e0e35a7697c0f8c17b03
Sathira Liyanapathirana <74991806+Sathira443@users.noreply.github.com>
2023-04-23 19:50:21 +0000

    Change the photos and Info in about me page

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Sathira Liyanapathirana",
+  title: "Full Stack Developer",
+  email: "liyanapathiranasathira@gmail.com",
+  gitHub: "Sathira443",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  linkedIn: "sathira-liyanapathirana",
+  medium: "@sathira98"
 };
 
 const primaryColor = "#4E567E";
